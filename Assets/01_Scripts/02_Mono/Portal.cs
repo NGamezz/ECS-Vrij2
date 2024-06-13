2e2beba8748e6bcb8cf7a45ef2289eaffd5d8ec6
NGamezz <>
2024-06-13 03:59:14 +0200

    Slight Improvements.

--- Assets/01_Scripts/02_Mono/Portal.cs
+++ Assets/01_Scripts/02_Mono/Portal.cs
@@ -6,14 +6,19 @@ public class Portal : MonoBehaviour
 {
     [SerializeField] private UnityEvent uponEnablePortal;
 
+    [SerializeField] private float range = 10.0f;
+
     [SerializeField] private UnityEvent uponPortalActivation;
 
     [SerializeField] private InputAction activationAction;
+
+    private Transform playerTransform;
 
     private bool active = true;
 
     private void Start ()
     {
+        playerTransform = FindAnyObjectByType<PlayerMesh>().GetTransform();
         activationAction.performed += Activate;
     }
 
@@ -26,6 +31,15 @@ public class Portal : MonoBehaviour
     private void Activate ( InputAction.CallbackContext ctx )
     {
         if ( !active )
+            return;
+
+        var playerPos = playerTransform.position;
+        var ownPos = transform.position;
+
+        playerPos.y = 0.0f;
+        ownPos.y = 0.0f;
+
+        if ( Vector3.Distance(playerPos, ownPos) > range )
             return;
 
         uponPortalActivation?.Invoke();
